f850aa5a89e0f54a739f2600e85456084cac9b92
Mrutyunjaya Pradhan <46385035+mpgbk@users.noreply.github.com>
2020-04-27 01:19:16 +0530

    connect mongodb to app

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//Connect to mongoDB database
+connectDB();
 
 app.get('/api/sample', (req, res) => {
   const sampleData = [
