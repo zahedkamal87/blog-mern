48f96798b721abd8b7dd5b1994045044c156b218
Mrutyunjaya Pradhan <46385035+mpgbk@users.noreply.github.com>
2020-04-26 21:45:01 +0530

    add linting and formatting

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
-
-app.get('/api/sample', (req, res)=>{
+app.get('/api/sample', (req, res) => {
   const sampleData = [
     {
-      id:1,
+      id: 1,
       firstName: 'Brahma'
     },
     {
-      id:2,
+      id: 2,
       firstName: 'Vishnu'
     },
     {
-      id:3,
+      id: 3,
       firstName: 'Maheswar'
     }
-  ]
-  res.json(sampleData)
-})
+  ];
+  res.json(sampleData);
+});
 
 const PORT = 5000;
 
-app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
