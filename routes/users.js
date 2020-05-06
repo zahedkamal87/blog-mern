41a7fd1a0f87430e80e210b2fe2ec2e8f2ba33ff
Mrutyunjaya Pradhan <46385035+mpgbk@users.noreply.github.com>
2020-05-06 20:19:10 +0530

    authorize user and get jwt

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator');
+
 const User = require('../models/User');
 
 // @route   POST /api/users
