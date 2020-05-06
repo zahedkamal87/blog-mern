538567b17a967c72f42a3c086856b9714d55fc96
Mrutyunjaya Pradhan <46385035+mpgbk@users.noreply.github.com>
2020-05-06 20:58:16 +0530

    add auth in private route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middlewares/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @route   GET /api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    let user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST /api/auth
